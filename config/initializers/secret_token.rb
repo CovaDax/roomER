ff256c8c5c557efee3d69517c845ed9cda7664f7
***infinite recursion in variable git_username*** <***infinite recursion in variable git_email***>
2013-10-28 00:12:36 -0400

    Empty Website HomeRooted

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CalTest::Application.config.secret_token = '9f2ae972738840837c020c8b4e7d8f3257a79446c84919c22f3bfa45f9f9447fd3d0fd4fecad70d57e1cbeef78c160a4cbefdc86f642d0054cb7033e9405d33a'
