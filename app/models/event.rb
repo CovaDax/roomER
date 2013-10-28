712344f5f4d8acd09151763d7b268406dad5cadd
***infinite recursion in variable git_username*** <***infinite recursion in variable git_email***>
2013-10-28 04:33:10 -0400

    added Room to Events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,4 @@
 class Event < ActiveRecord::Base
-  attr_accessible :end_at, :name, :start_at, :organization
+  attr_accessible :end_at, :name, :start_at, :organization, :room
   has_event_calendar
 end
