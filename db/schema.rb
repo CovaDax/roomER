756e6f69ae8ad50c44f28279bfe19d7a5b551a9c
***infinite recursion in variable git_username*** <***infinite recursion in variable git_email***>
2013-10-28 03:59:50 -0400

    added Organization to Events

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131028060136) do
+ActiveRecord::Schema.define(:version => 20131028074032) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
     t.datetime "start_at"
     t.datetime "end_at"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+    t.string   "organization"
   end
 
 end
