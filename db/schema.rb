4fcdd13772506ce580cedaf371339d6e8a34ca2d
Derek McCan <damccan@eagle.fgcu.edu>
2013-12-08 18:22:11 -0500

    Stylesheet changes

--- db/schema.rb
+++ db/schema.rb
@@ -13,24 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20131208011241) do
 
-  create_table "admins", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-  end
-
-  add_index "admins", ["email"], :name => "index_admins_on_email", :unique => true
-  add_index "admins", ["reset_password_token"], :name => "index_admins_on_reset_password_token", :unique => true
-
   create_table "controllers", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
@@ -58,13 +40,13 @@ ActiveRecord::Schema.define(:version => 20131208011241) do
     t.datetime "updated_at",   :null => false
     t.string   "organization"
     t.integer  "room_id"
+    t.boolean  "approved"
+    t.integer  "numAttendees"
     t.boolean  "validated"
     t.integer  "numAttends"
     t.string   "contactName"
     t.string   "email"
     t.string   "eventName"
-    t.boolean  "approved"
-    t.integer  "numAttendees"
   end
 
   create_table "models", :force => true do |t|
@@ -95,8 +77,8 @@ ActiveRecord::Schema.define(:version => 20131208011241) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",    :null => false
-    t.string   "encrypted_password",     :default => "",    :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -105,11 +87,10 @@ ActiveRecord::Schema.define(:version => 20131208011241) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                :null => false
-    t.datetime "updated_at",                                :null => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.string   "name"
     t.string   "username"
-    t.boolean  "admin",                  :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
