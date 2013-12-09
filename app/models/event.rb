dd17d9d90f060e18a11a2405b3a862ad425ff676
***infinite recursion in variable git_username*** <***infinite recursion in variable git_email***>
2013-12-09 08:22:38 -0500

    added contactEmail validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,16 +6,20 @@ class Event < ActiveRecord::Base
   belongs_to :room
   validates_presence_of :name,:organization,:numAttends, :if => lambda{|o| o.current_step == "first"}
   validates :numAttends, :numericality => {:greater_than => 0}, :if => lambda{|o| o.current_step == "first"}
-  validates_presence_of :contactName, :contactEmail, :contactPhone, :if => lambda{|o| o.current_step == "fourth"}
   #validate :sensible_datetime?  Does not work with MultiStep form
-  
-  
+  validates_presence_of :contactName, :contactPhone, :if => lambda{|o| o.current_step == "fourth"}
+  validates :contactEmail,   
+            :presence => true,   
+            :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }   ,
+         	:if  => lambda{|o| o.current_step == "fourth"}
   
   def sensible_datetime?
   if self.start_at > self.end_at
       errors.add(:ends_at, 'must be after start time')
     end
   end
+
+
   
 
 
